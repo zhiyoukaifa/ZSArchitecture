b7a1ca260651883bdf0a1c52082db3a5b12292ad
zhangsan <zhangan@ejiajx.com>
2018-10-09 22:26:40 +0800

    添加App 信息类

---
+++ ZSArchitecture/ZSArchitecture/CommonTools/DeviceTools/ZSAppBaseMessage.h
@@ -0,0 +1,13 @@
+//
+//  ZSAppBaseMessageTool.h
+//  ZSArchitecture
+//
+//  Created by 道道明明白白 on 2018/10/9.
+//  Copyright © 2018年 道道明明白白. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZSAppBaseMessageTool : NSObject
+
+@end
---
+++ ZSArchitecture/ZSArchitecture/CommonTools/DeviceTools/ZSAppBaseMessage.m
@@ -0,0 +1,13 @@
+//
+//  ZSAppBaseMessageTool.m
+//  ZSArchitecture
+//
+//  Created by 道道明明白白 on 2018/10/9.
+//  Copyright © 2018年 道道明明白白. All rights reserved.
+//
+
+#import "ZSAppBaseMessageTool.h"
+
+@implementation ZSAppBaseMessageTool
+
+@end
